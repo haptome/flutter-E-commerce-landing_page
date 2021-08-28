966b2c53aa469901d9e73ff907d15053508453d7
haptome <haptome2@icloud.com>
2021-08-28 08:30:19 +0300

    Create style.dart
    style class

---
+++ lib/Helper/style.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+Color disable = Color(0xFF7D8790);
+Color brokenWhite = Color(0xFFEEEBEE);
+Color bg = Color(0xFFE8E5E9);
+Color disableText = Colors.grey.shade500;
+Color normalText = Colors.black;
